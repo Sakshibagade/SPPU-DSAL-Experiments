5c04e0c35d6089ff0c62e9b7eae4ec91459867a5
SakshiB <134160784+Sakshibagade@users.noreply.github.com>
2023-06-08 14:35:05 +0530

    pr4.cpp

--- pr4.cpp
+++ pr4.cpp
@@ -1,3 +1,11 @@
+/* Practical No-4
+Beginning with an empty binary search tree, Construct binary search tree by inserting the values in the order given. After constructing a binary tree - Insert new node
+●	Find number of nodes in longest path from root  
+●	Minimum data value found in the tree
+●	Change a tree so that the roles of the left and
+right pointers are swapped at every node
+●	Search a value */
+
 #include <iostream>
 using namespace std;
 
